830228803e50e6f1e8a18ac9a000555c5c50c7f2
pandora-site <b1sampai112@gmail.com>
2026-07-30 05:43:38 +0800

    Add service worker with basic caching functionality

---
+++ sw.js
@@ -0,0 +1,1 @@
+var C='pandora';self.addEventListener('install',function(e){self.skipWaiting()});self.addEventListener('activate',function(e){clients.claim()});self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
